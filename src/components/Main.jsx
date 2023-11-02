72889d2de81d3131f584acf108040faac4b1cc7f
Mariana Velázquez <124692121+28999Mariana@users.noreply.github.com>
2023-11-02 20:49:40 -0300

    Add files via upload

---
+++ src/components/Main.jsx
@@ -0,0 +1,35 @@
+import './main.css'
+
+export const Main = () => {
+
+
+  return(
+    <main>
+      <section className="intro-section">
+        <h3>Hi! I'm Mariana!</h3>
+        <h1> Welcome to my Portfolio.</h1>
+        <p>I'm a seasoned Frontend Developer known for my expertise in UI/UX design. My approach combines technical prowess with a creative eye, resulting in captivating digital experiences. With a track record of pushing boundaries and a passion for innovation, I'm dedicated to creating websites and applications that not only look exceptional but also function flawlessly. Let's work together to bring your vision to life and set new standards in user experience.</p>
+        <img src="../assets/img/astronauta.svg" alt="Imagen de astronauta" />
+      </section> 
+      <section className="section-2"> 
+      <div className="carousel-habilidades">
+        <img src=""></img>
+        <img src=""></img>
+        <img src=""></img>
+      </div>
+      </section>
+      <section className="section-3">
+        <div className="projects">
+        <img src=""></img>
+        <img src=""></img>
+        <img src=""></img>
+        <img src=""></img>
+        <img src=""></img>
+        <img src=""></img>
+
+        </div>
+      </section> 
+
+    </main>
+  )
+}
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <h3>Mariana Soledad Velázquez</h3>
+    </div>
+  );
+}
